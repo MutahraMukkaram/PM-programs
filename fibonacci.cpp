e5e0350ce2265e185ce123a7e32c6b33712e90bc
MutahraMukkaram <mutaharamukkaram@gmail.com>
2026-04-10 23:45:44 +0530

    Implement Fibonacci series generator in C++

---
+++ fibonacci.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void fibonacci(int n)
+{
+    int a = 0, b = 1, c, i;
+    cout << "Fibonacci Series: ";
+    for (i = 1; i <= n; i++)
+    {
+        cout << a << " ";
+        c = a + b;
+        a = b;
+        b = c;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+    fibonacci(n);
+    return 0;
+}
